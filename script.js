2987b2a6a8a4c421290e814eca265d71cb6c866c
sarim ali <sarimshaikh597@gmail.com>
2026-01-08 13:42:29 +0500

    handle out going user message

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const chatBody = document.querySelector(".chat-body");
 const messageInput = document.querySelector(".message-input");
+const sendMessageButton = document.querySelector("#send-message");
+const userData = {
+    message: null
+}
 // create message element with dynamic classes and return it 
 const createMessageElement = (content, classes) => {
     const div = document.createElement("div");
@@ -8,8 +12,11 @@ const createMessageElement = (content, classes) => {
     return div;
 }
 // handle out going user message
-const handleOutgoingMessage = (userMessage) => {
-    const messageContent = `<div class="message-text">${userMessage}</div>`;
+const handleOutgoingMessage = (e) => {
+    e.preventDefault();
+    userData.message = messageInput.value.trim();
+    // create and display user message
+    const messageContent = `<div class="message-text">${userData.message}</div>`;
     const outgoingMessageDiv = createMessageElement(messageContent, "user-message");
     chatBody.appendChild(outgoingMessageDiv);
 }
@@ -17,6 +24,7 @@ const handleOutgoingMessage = (userMessage) => {
 messageInput.addEventListener("keydown", (e) => {
     const userMessage = e.target.value.trim();
     if (e.key === "Enter" && userMessage) {
-        handleOutgoingMessage(userMessage);
+        handleOutgoingMessage(e);
     }
 })
+sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e))
